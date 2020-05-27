8c27280254c4bc978930ea176326095de16799d5
JulianFun123 <julianfun123@yahoo.com>
2020-05-27 12:41:49 +0200

    Added simple terms

---
+++ resources/compile/views/terms.view.php
@@ -0,0 +1,40 @@
+@template(("header", ["title"=>"DSGVO"]))!
+<app>
+<div id="homepagetopsection">
+    <div id="homepagetopsectioninner">
+        <a id="homepagetopslogan">{{ ulole\core\classes\Lang::lang("terms") }}</a>
+    </div>
+</div>
+<br><br><br>
+<div class="lightsection">
+    
+<p>
+</p>
+<br><br><br>
+<h1>Guidelines</h1>
+<h2>General Guidlines</hh>
+<p>
+    You are not allowed to share this content in all of our services:
+    <ul>
+        <li>Spam</li>
+        <li>Pornography</li>
+        <li>Copyrighted content</li>
+    </ul>
+    <br>
+    By not following the rules you'll have to accept that your content or account will be blocked or deleted.
+</p>
+
+
+
+</div>
+<style>
+
+    #homepagetopsectioninner {
+        padding-top: 120px;
+        padding-bottom: 100px;
+    }
+
+</style>
+
+</app>
+@template(("footer"))!
---
+++ resources/views/terms.php
@@ -0,0 +1,40 @@
+<?php tmpl("header", ["title"=>"DSGVO"]); ?>
+<app>
+<div id="homepagetopsection">
+    <div id="homepagetopsectioninner">
+        <a id="homepagetopslogan"><?php echo ( ulole\core\classes\Lang::lang("terms") ); ?></a>
+    </div>
+</div>
+<br><br><br>
+<div class="lightsection">
+    
+<p>
+</p>
+<br><br><br>
+<h1>Guidelines</h1>
+<h2>General Guidlines</hh>
+<p>
+    You are not allowed to share this content in all of our services:
+    <ul>
+        <li>Spam</li>
+        <li>Pornography</li>
+        <li>Copyrighted content</li>
+    </ul>
+    <br>
+    By not following the rules you accept that the content or your account will be blocked or deleted.
+</p>
+
+
+
+</div>
+<style>
+
+    #homepagetopsectioninner {
+        padding-top: 120px;
+        padding-bottom: 100px;
+    }
+
+</style>
+
+</app>
+<?php tmpl("footer"); ?>
